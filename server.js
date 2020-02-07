a8557e12e6b35ee28b7228962194eda6bce0ca46
Ricky Gonzalez <rickygonzalez@gmail.com>
2020-02-07 01:56:19 -0600

    created pre-database signing function

--- server.js
+++ server.js
@@ -1,24 +1,43 @@
-const express = require('express')
+const express = require("express");
 
 const app = express();
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + "/public"));
 
-app.post('/signin', (req, res) => {
-
-})
+const database = {
+  users: [
+    {
+    id: 1,
+    name: "John",
+    email: "john@example.com",
+    password: "password",
+    entries: 0,
+    joined: new Date()
+    } 
+  ]
+};
+app.post("/signin", (req, res) => {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json("success");
+  } else {
+    res.status(400).json("error logging in");
+  }
+});
 
 /* /signin -> POST success/fail
  * /register -> POST success => return user
- * /profile/:userId -> GET user 
+ * /profile/:userId -> GET user
  * /image -> Put -> user => update user.count
- * 
- * 
- */ 
+ *
+ *
+ */
 
-app.listen(3000, () =>{
-    console.log('FaceBrain api running on port 3000')
-})
+app.listen(3000, () => {
+  console.log("FaceBrain api running on port 3000");
+});
